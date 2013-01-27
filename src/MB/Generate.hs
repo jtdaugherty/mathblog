18b491311bce74ca37e5f3bc018dfb703db6a335
Jonathan Daugherty <cygnus@foobox.com>
2013-01-27 15:21:40 -0800

    Bugfix: report error on blog data directory with no posts

--- src/MB/Generate.hs
+++ src/MB/Generate.hs
@@ -117,6 +117,9 @@ generatePosts blog summary = do
 
 buildIndexPage :: Blog -> IO ()
 buildIndexPage blog = do
+  when (null $ blogPosts blog) $
+       error "No blog posts; please create a first post in posts/."
+
   let src = Files.postFinalHtml blog post
       index = Files.indexHtml blog
       post = head $ blogPosts blog
